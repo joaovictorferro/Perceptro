d4d7f9068b4de8e27ebae96a8e5ded88ea5d273e
João Victor Ribeiro Ferro <39158742+joaovictorferro@users.noreply.github.com>
2021-09-02 15:14:50 -0300

    att

--- main.py
+++ main.py
@@ -1,68 +1,46 @@
 #Equipe:
-# Arthur Savio
-# Guilherme Amaral
-# Joao Victor Ribeiro
-#Tabela 15
+#Arthur Savio
+#Guilherme Amaral
+#João Victor Ribeiro
+#tabela: 15
 
-entrada = \
-    [[1,1,1],
-     [1,1,0],
-     [1,0,1],
-     [1,0,0],
-     [0,1,1],
-     [0,1,0],
-     [0,0,1],
-     [0,0,0]]
-peso = [0.3,0.3,0.3]
-resposta_esperada=[1,1,1,1,1,1,0,1]
+import pandas as pd
+from sklearn.neural_network import MLPClassifier
 
-def pergunta():
-    soma = 0.0
-    variavel = []
-    variavel.append(float(input("Digite o valor de q1\n")))
-    variavel.append(float(input("Digite o valor de q2\n")))
-    variavel.append(float(input("Digite o valor de q3\n")))
 
-    for j in range(len(peso)):
-        # print(Entrada[i])
-        soma += peso[j] * variavel[j]
-    # Funcao degrau
-    if soma < 0:
-        saida = 0
-    else:
-        saida = 1
+table15 = [[1, 1, 1, 1],# 1 pra True e 0 pra false
+            [1, 1, 0, 1],
+            [1, 0, 1, 1],
+            [1, 0, 0, 1],
+            [0, 1, 1, 1],
+            [0, 1, 0, 1],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]]
 
-    print("A resposta obtida foi " + str(saida))
+columns = ["p", "q", "r", "(p or r) or !q"]
+features = ["p", "q", "r"]
+result = ["(p or r) or !q"]
 
-def treinamento():
-    cont = 0
-    while True:
-        loop = True
-        print ("EPOCA: "+ str(cont))
-        for i in range(len(entrada)):
-            soma = 0.0
-            for j in range(len(peso)):
-                #print(Entrada[i])
-                soma += entrada[i][j] * peso[j]
-            print("SOMA: " + str(soma))
-            #Funcao degrau
-            if soma < 0:
-                saida = 0
-            else:
-                saida = 1
+df = pd.DataFrame(data = table15, columns = columns)
 
-            erro = resposta_esperada[i] - saida
+X = df[features]
+y = df[result]
 
-            print("ERROR: " + str(erro))
-            if erro != 0:
-                loop = False
-                for r in range(len(peso)):
-                    peso[r] = peso[r] + 0.1 * (erro * entrada[i][r])
-        cont += 1
-        if loop:
-            break;
+clf = MLPClassifier(solver='sgd', activation = 'logistic', hidden_layer_sizes=(9, 9), learning_rate_init = 1,
+                    learning_rate = 'constant', random_state=1,max_iter=500)
 
-if __name__ == '__main__':
-    treinamento()
-    print(peso)
-    pergunta()
+clf.fit(X, [1, 1, 1, 1, 1, 1, 0, 1])
+
+
+test = [[1, 1, 1],
+            [1, 1, 0],
+            [1, 0, 1],
+            [1, 0, 0],
+            [0, 1, 1],
+            [0, 1, 0],
+            [0, 0, 1],
+            [0, 0, 0]]
+
+clf.predict(test)
+
+print(clf.score(test,[1,1,1,1,1,1,0,1]))
