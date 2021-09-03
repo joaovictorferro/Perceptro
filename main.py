e77a14c64db032524616acbd55a391290096b809
João Victor Ribeiro Ferro <39158742+joaovictorferro@users.noreply.github.com>
2021-09-03 11:22:33 -0300

    att

--- main.py
+++ main.py
@@ -14,69 +14,52 @@ entrada = \
      [0,0,1],
      [0,0,0]]
 peso = [0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3]
-resposta_esperada=[0,1,1,1,1,1,0,1]
+resposta_esperada=[1,1,1,1,1,1,0,1]
 
-def not_camada_5(matriz_resposta):
-    soma = 0.0
-    soma += matriz_resposta[0][3] * peso[8]
-    # print("SOMA: " + str(soma))
-    # Funcao degrau
+def funcao_degrau(soma):
     if soma < 0:
         saida = 0
     else:
         saida = 1
-
     return saida
+
+def not_camada_5(matriz_resposta):
+    soma = 0.0
+    soma += matriz_resposta[0][3] * peso[8]
+
+    # Funcao degrau
+    return funcao_degrau(soma)
 
 def or_and_camada_4(matriz_resposta):
     soma = 0.0
     soma += matriz_resposta[0][2] * peso[6]
     soma += matriz_resposta[2][0] * peso[7]
-    # print("SOMA: " + str(soma))
-    # Funcao degrau
-    if soma < 0:
-        saida = 0
-    else:
-        saida = 1
 
-    return saida
+    # Funcao degrau
+    return funcao_degrau(soma)
 
 def not_camada_3(matriz_resposta):
     soma = 0.0
     soma += matriz_resposta[0][1] * peso[5]
-    # print("SOMA: " + str(soma))
-    # Funcao degrau
-    if soma < 0:
-        saida = 0
-    else:
-        saida = 1
 
-    return saida
+    # Funcao degrau
+    return funcao_degrau(soma)
 
 def or_and_camada_2(matriz_resposta):
     soma = 0.0
     soma += matriz_resposta[0][0] * peso[3]
     soma += matriz_resposta[1][0] * peso[4]
-    # print("SOMA: " + str(soma))
-    # Funcao degrau
-    if soma < 0:
-        saida = 0
-    else:
-        saida = 1
 
-    return saida
+    # Funcao degrau
+    print("SOMA segunda camada: " + str(soma))
+    return funcao_degrau(soma)
 
 def not_camada_1(i,j):
     soma = 0.0
     soma += entrada[i][j] * peso[j]
-    # print("SOMA: " + str(soma))
-    # Funcao degrau
-    if soma < 0:
-        saida = 0
-    else:
-        saida = 1
 
-    return saida
+    # Funcao degrau
+    return funcao_degrau(soma)
 
 def treinamento():
     epoca = 0
@@ -109,8 +92,10 @@ def treinamento():
             print("ERROR: " + str(erro))
             if erro != 0:
                 loop = False
-                for r in range(3):
-                    peso[r] = peso[r] + 0.1 * erro * entrada[i][r]
+                print("Entrada : " + str(entrada[i]))
+                peso[0] = peso[0] + 0.1 * erro * entrada[i][0]
+                peso[1] = peso[1] + 0.1 * erro * entrada[i][1]
+                peso[2] = peso[2] + 0.1 * erro * entrada[i][2]
                 peso[3] = peso[3] + 0.1 * erro * matriz_resposta[0][0]
                 peso[4] = peso[4] + 0.1 * erro * matriz_resposta[1][0]
                 peso[5] = peso[5] + 0.1 * erro * matriz_resposta[0][1]
